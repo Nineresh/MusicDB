61bc45cc4c162c927a4fdab64cadd53d898bf6b6
Nineresh <1990andreaslindblad@gmail.com>
2024-02-21 10:17:25 +0100

    Started importing small codesnippet from previous version into test folder

--- test/main_test.py
+++ test/main_test.py
@@ -1,6 +1,20 @@
 import database
-print("Initialize")
+import os
+def main():
+    pass
+#Create list of subfolders
+def lista_mappar(target_folder):
+    submappar = []
+# Använder 'os'-bibloteket för att läsa av alla submappar i argumentet
+# sökvägen som läses in.
+    for k in os.listdir(target_folder):
+        
+        submappar.append(k)
+#returnerar en lista som innehåller alla submappar
+    return submappar
+
+# Connecting to database, refrence in database.py
 connection = database.connect()
 
-    
-print ("Done")
+if __name__ == "__main__":
+    pass
