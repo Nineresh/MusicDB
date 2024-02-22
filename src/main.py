f4bacc6d3f94bc7ce71d1aa099559abdc58e4063
Nineresh <1990andreaslindblad@gmail.com>
2024-02-22 22:12:59 +0100

    launch 1.0 src folder _active_

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import os
 import glob
 import eyed3
 import logging
+
 print("Starting")
-#Logging section
+
+# Logging section
 LOG_FORMAT = ("%(asctime)s, %(lineno)d, %(levelname)s, %(message)s")
 logging.basicConfig(filename="resources/main.log",
                     level=logging.DEBUG,
@@ -12,28 +14,66 @@ logging.basicConfig(filename="resources/main.log",
                     format=LOG_FORMAT)
 logger = logging.getLogger()
 
-#Database section
-conn = sqlite3.connect("test_musicdatabase.db")
+count_track = 0
+count_album = 0
+list_album = []
+count_artist = 0
+list_artist = []
+
+# Database section
+conn = sqlite3.connect("database/musicdatabase.db")
 c = conn.cursor()
 
 logger.debug("Starting program")
-#Subfolder is a list of subfolders in musik
+
+# Subfolder is a list of subfolders in musik
 subfolder = []
 for k in os.listdir("/Users/andreaslindblad/documents/musik"): 
     subfolder.append(k)
-
 
 for folder in range (0,len(subfolder)):
     mp3_path= glob.glob(os.path.join("/Users/andreaslindblad/documents/musik/"+subfolder[folder], "*.mp3"))
     for mp3 in mp3_path:
         audiofile = eyed3.load(mp3)
-        c.execute("""INSERT INTO tracks(title, album, artist) VALUES (?,?,?)""",(audiofile.tag.title, audiofile.tag.album, audiofile.tag.artist))
-        print("Added:")
-        print("Track: "+ audiofile.tag.title)
-        print("Album: "+ audiofile.tag.album)
-        print("Artist: "+ audiofile.tag.artist)
-        #print("Year: "+ audiofile.tag.best_release_date)
-        conn.commit()
+        # Get tags in a varibel
+        tag = audiofile.tag
+
+        # Use the GetBestDate method. ISSUE TO BE FIXED
+        year_tag = tag.getBestDate()
+        string_year = str(year_tag)
+
+        # Check if the track already exists in the database
+        c.execute("""SELECT * FROM tracks WHERE title=? AND album=? AND artist=?""",
+                  (audiofile.tag.title, audiofile.tag.album, audiofile.tag.artist))
+        existing_track = c.fetchone()
+
+        if not existing_track:
+            c.execute("""INSERT INTO tracks(title, album, artist, year) VALUES (?,?,?,?)""",
+                      (audiofile.tag.title, audiofile.tag.album, audiofile.tag.artist, string_year))
+            print("Added:")
+            print("Track: " + audiofile.tag.title)
+            count_track += 1
+            print("Album: " + audiofile.tag.album)
+            if audiofile.tag.album not in list_album:
+                list_album.append(audiofile.tag.album)
+                count_album += 1
+            else:
+                pass
+            print("Artist: " + audiofile.tag.artist)
+
+            if audiofile.tag.artist not in list_artist:
+                list_artist.append(audiofile.tag.artist)
+                count_artist += 1
+            else:
+                pass
+
+            print("Year: " + string_year)
+            conn.commit()
+        else:
+            print("Track already exists: " + audiofile.tag.title)
+
 conn.close()
-print("Ending")
-logger.debug("Ending")
+print("You added:")
+print(count_track, " tracks")
+print(count_album, " albums")
+print(count_artist, " artists")
